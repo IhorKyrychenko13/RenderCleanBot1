f42687d759472002135696cf9c277971a8dba571
IhorKyrychenko13 <legouflandia@gmail.com>
2025-06-30 18:35:50 +0200

    Add files via upload

--- web_alive.py
+++ web_alive.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, Request
 from aiogram import types
-from bot import dp
+from bot import dp  # импорт твоего Dispatcher
 
 app = FastAPI()
 
-@app.post("/webhook_path")
+@app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
     update = types.Update(**data)
-    await dp.feed_update(update)
+    await dp.process_update(update)  # <-- правильно вызываем процессинг обновления
     return {"ok": True}
+
 @app.get("/")
 async def root():
     return {"status": "ok"}
