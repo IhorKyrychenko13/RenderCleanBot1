6950b7141dc099ed868491e1de081a8e028ed030
IhorKyrychenko13 <legouflandia@gmail.com>
2025-06-30 17:30:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-
 import asyncio
 import uvicorn
 from multiprocessing import Process
 from bot import run_bot
 from web_alive import app
+import os
 
 def run_api():
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     p = Process(target=run_api)
