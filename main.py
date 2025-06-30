aff18650400ff487036715d304a8e27a20fabb37
IhorKyrychenko13 <legouflandia@gmail.com>
2025-06-30 19:26:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+from aiogram import Bot, Dispatcher, types
 import os
 from fastapi import FastAPI, Request
-from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 import uvicorn
-from bot import router  # импорт роутера из bot.py
+from bot import router  # твой роутер с хендлерами
 
 load_dotenv()
 
@@ -12,7 +12,8 @@ if not TOKEN:
     raise RuntimeError("TOKEN is not set in environment variables")
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()  # БЕЗ параметров
+
 dp.include_router(router)
 
 app = FastAPI()
@@ -30,5 +31,4 @@ async def root():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    print(f"Запуск uvicorn на порту {port}...")
     uvicorn.run("main:app", host="0.0.0.0", port=port)
