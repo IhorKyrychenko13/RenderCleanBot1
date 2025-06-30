76aca3ffd9bfcf95e20b9e67ec8af8dade2a96bd
IhorKyrychenko13 <legouflandia@gmail.com>
2025-06-30 18:32:36 +0200

    Add files via upload

--- web_alive.py
+++ web_alive.py
@@ -4,9 +4,12 @@ from bot import dp
 
 app = FastAPI()
 
-@app.post("/webhook")
+@app.post("/webhook_path")
 async def webhook(request: Request):
     data = await request.json()
     update = types.Update(**data)
     await dp.feed_update(update)
     return {"ok": True}
+@app.get("/")
+async def root():
+    return {"status": "ok"}
